33ded55f36eb92bd2854da93dde19134368ce93f
Karl Lindmark <karl.lindmark@acino.se>
2016-03-31 13:54:22 +0200

    Make the buttons on / stack nicely on smaller screens

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,8 @@
 import {Component} from 'angular2/core';
 
 @Component({
-  template: require('./home.html')
+  template: require('./home.html'),
+  styles: [ require('./home.css') ]
 })
 
 export class Home {
